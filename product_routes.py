5173c3590f4f3ec547191be3cd1fb54c13584ca7
wmcsantos <wilsoncavacosantos@gmail.com>
2025-04-02 23:18:24 +0100

    Product Routes Added

--- product_routes.py
+++ product_routes.py
@@ -2,12 +2,12 @@ from fastapi import Depends, APIRouter, HTTPException, status
 from typing import List
 from sqlalchemy.orm import Session
 from .database import get_db
-from .models import ProductVariants, Sizes, ColorProducts, Colors, Products
-from .schemas import ProductVariantResponse
+from .models import ProductVariants, Sizes, ColorProducts, Colors, Products, ProductImages, Categories, t_category_products
+from .schemas import ProductVariantResponse, ProductsByCategoryResponse, ProductDetailResponse, ProductImageResponse, ProductColorResponse
 
 product_router = APIRouter(
-    prefix = '/product',
-    tags = ['product']
+    prefix = '/products',
+    tags = ['products']
 )
 
 @product_router.get("/variants", response_model=List[ProductVariantResponse])
@@ -41,3 +41,106 @@ def get_product_variants(
 
     # Convert query results into dictionaries
     return variants
+
+@product_router.get("/category/{id}", response_model=list[ProductsByCategoryResponse])
+def get_products_by_category(id: int, db: Session = Depends(get_db)):
+    products = (
+        db.query(
+            ProductImages.position,
+            ProductImages.image_url,
+            ColorProducts.product_id,
+            Colors.code,
+            Colors.image_url,
+            Products.name,
+            ProductVariants.price,
+            Categories.id,
+            Categories.name
+        )
+        .distinct(ColorProducts.product_id, Categories.id, Colors.code)
+        .outerjoin(ColorProducts, ProductImages.color_products_id == ColorProducts.id)
+        .outerjoin(Colors, Colors.id == ColorProducts.color_id)
+        .outerjoin(Products, Products.id == ColorProducts.product_id)
+        .outerjoin(ProductVariants, ProductVariants.product_id == ColorProducts.product_id)
+        .outerjoin(t_category_products, t_category_products.c.product_id == ColorProducts.product_id)
+        .outerjoin(Categories, Categories.id == t_category_products.c.category_id)
+        .filter(ProductImages.position == 1, Categories.id == id)
+        .all()
+    )
+
+    return [
+        ProductsByCategoryResponse(
+            position=p[0],
+            image_url=p[1],
+            product_id=p[2],
+            color_code=p[3],
+            color_image_url=p[4],
+            product_name=p[5],
+            price=p[6],
+            category_id=p[7],
+            category_name=p[8]
+        )
+        for p in products
+    ]
+
+@product_router.get("/product/{id}", response_model=list[ProductDetailResponse])
+def get_product_by_id(id: int, db: Session = Depends(get_db)):
+    product = (
+        db.query(
+            Products.id,
+            Products.name,
+            Products.description_details,
+            Products.description_composition,
+            Products.description_care,
+            Products.description_delivery,
+            ProductVariants.id.label("product_variant_id"),
+            ProductVariants.price
+        )
+        .join(ProductVariants, Products.id == ProductVariants.product_id)
+        .filter(Products.id == id)
+        .all()
+    )
+
+    if not product:
+        raise(HTTPException(status_code=404, detail="Product not found"))
+
+    return product
+
+@product_router.get("/product-images", response_model=list[ProductImageResponse])
+def get_product_images(color_code: str, product_id: int, db: Session = Depends(get_db)):
+    product_images = (
+        db.query(
+            ProductImages.id,
+            ProductImages.image_url,
+            ProductImages.position,
+            Colors.code
+        )
+        .join(ColorProducts, ProductImages.color_products_id == ColorProducts.id)
+        .join(Colors, ColorProducts.color_id == Colors.id)
+        .filter(Colors.code == color_code, ColorProducts.product_id == product_id)
+        .order_by(ProductImages.position.asc())
+        .all()
+    )
+
+    if not product_images:
+        raise HTTPException(status_code=404, detail="No product images found")
+    
+    return product_images
+
+@product_router.get("/product-colors", response_model=list[ProductColorResponse])
+def get_product_images(product_id: int, db: Session = Depends(get_db)):
+    product_colors = (
+        db.query(
+            Colors.name,
+            Colors.code,
+            Colors.image_url,
+            ColorProducts.color_id
+        )
+        .join(ColorProducts, ColorProducts.color_id == Colors.id)
+        .filter(ColorProducts.product_id == product_id)
+        .all()
+    )
+
+    if not product_colors:
+        raise HTTPException(status_code=404, detail="No product colors found")
+    
+    return product_colors
--- schemas.py
+++ schemas.py
@@ -29,6 +29,39 @@ class ProductVariantResponse(BaseModel):
     created_at: datetime.datetime
     updated_at: Optional[datetime.datetime]
 
+class ProductsByCategoryResponse(BaseModel):
+    position: int
+    image_url: str
+    product_id: int
+    color_code: str
+    color_image_url: str
+    product_name: str
+    price: float
+    category_id: int
+    category_name: str
+
 class CategoryResponse(BaseModel):
     id: int
     name: str
+
+class ProductDetailResponse(BaseModel):
+    id: int
+    name: str
+    description_details: Optional[str]
+    description_composition: Optional[str]
+    description_care: Optional[str]
+    description_delivery: Optional[str]
+    product_variant_id: int
+    price: float
+
+class ProductImageResponse(BaseModel):
+    id: int
+    image_url: str
+    position: int
+    code: str
+
+class ProductColorResponse(BaseModel):
+    name: str
+    code: str
+    image_url: str
+    color_id: int
