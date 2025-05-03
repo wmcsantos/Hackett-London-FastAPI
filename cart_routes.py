ad71746cc286e2cec96e6f3d5d45e60f5fd98f97
wmcsantos <wilsoncavacosantos@gmail.com>
2025-05-03 22:44:34 +0100

    Delete Item From Cart Route Added

--- cart_routes.py
+++ cart_routes.py
@@ -86,6 +86,25 @@ def delete_cart(
 
     return 
 
+@cart_router.delete("/cart/{cart_id}/{item_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_item_from_cart(
+    cart_id: int,
+    item_id: int,
+    db: Session = Depends(get_db),
+    current_user: Users = Depends(get_current_user)
+):
+    cart = db.query(Carts).filter(Carts.id == cart_id, Carts.user_id == current_user.id).first()
+
+    if not cart:
+        raise HTTPException(status_code=404, detail='Cart not found for this user')
+    
+    # First delete cart items
+    db.query(CartItems).filter(CartItems.id == item_id).delete()
+    
+    db.commit()
+
+    return 
+
 @cart_router.get("/cart/{cart_id}/cart-items/count")
 def count_cart_items(
     cart_id: int,
